7638edd57f76124300cbc246e0e886eddff2f719
梁振 <liangzhen.cn@gmail.com>
2015-06-22 22:38:44 +0800

    经过对terminal 的配置，solarized theme已经可以在终端中正常显示，然而比较了一下，并没有想象中的好用，反而自带的那个比较好用，所以我决定用回自带的。 捎带一句，换了source code pro 字体，感觉很不错

--- config/solarized-theme-config.el
+++ config/solarized-theme-config.el
@@ -1,3 +1,8 @@
-(add-to-list 'load-path "~/.emacs.d/plugin/solarized-theme")
-(add-to-list 'load-path "~/.emacs.d/plugin/dash")
+;;(add-to-list 'load-path "~/.emacs.d/plugin/solarized-theme")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/plugin/solarized-theme")
+(add-hook 'after-make-frame-functions
+          (lambda (frame)
+            (let ((mode (if (display-graphic-p frame) 'light 'dark)))
+              (set-frame-parameter frame 'background-mode mode)
+              (set-terminal-parameter frame 'background-mode mode))
+                        (enable-theme 'solarized)))
