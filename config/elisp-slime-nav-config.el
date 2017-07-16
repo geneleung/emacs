609c857cbe4089b57697ff83d10bf5e1e88f7b89
chappie <liangzhen.cn@gmail.com>
2017-07-17 01:56:36 +0800

    添加elpa管理插件，插件本身不放在git仓库中。同时添加org-page配置和elisp代码浏览配置

---
+++ config/elisp-slime-nav-config.el
@@ -0,0 +1,2 @@
+(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+  (add-hook hook 'elisp-slime-nav-mode))
--- init.el
+++ init.el
@@ -1,32 +1,22 @@
 ;; liangzhen's emacs24 config
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(define-obsolete-function-alias 'org-define-error 'define-error);;org-page编译时的错误处理
 ;;set defult loadpath
 (add-to-list 'load-path "~/.emacs.d/config")
 ;; load config
 (load "base.el")
-(load "cc-mode-config.el")
-(load "ztree.el")
-(load "ediff-config.el")
-(load "slime.el")
+(load "elisp-slime-nav-config.el")
+(load "org-page-config.el")
+;; (load "cc-mode-config.el")
+;; (load "ztree.el")
+;; (load "ediff-config.el")
+;; (load "slime.el")
 (load "yasnippet-config.el")
 (load "auto-complete-config.el")
-(load "android-config.el")
+;; (load "android-config.el")
 ;; solarized theme is not as cofortable as it says,but misterioso is more better
 ;; (load "solarized-theme-config.el")
 
-(autoload 'gtags-mode "gtags" "" t)
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (misterioso)))
- '(indent-tabs-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'narrow-to-region 'disabled nil)
+;; (autoload 'gtags-mode "gtags" "" t)
