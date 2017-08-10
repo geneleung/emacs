f14c0c67925a9ecefd3e170fa0a74a2725f1ccc3
chappie <zhen4.lz4@alibaba-inc.com>
2017-08-10 10:54:09 +0800

    换了emacs25之后老的ac总是报错，换成用MELPA安装稳定版本，就可以用了

--- config/auto-complete-config.el
+++
@@ -1,5 +0,0 @@
-(add-to-list 'load-path "~/.emacs.d/plugin/auto-complete")
-(require 'auto-complete-config)
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/plugin/auto-complete/ac-dict")
-(ac-config-default)
-
